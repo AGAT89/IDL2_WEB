a57cb00145acca1ea8b8d6f56261057ae0a4829a
AGAT89 <aariastevez@gmail.com>
2024-03-17 00:12:40 -0500

    Agregar archivos de proyecto.

---
+++ IDL2_WEB/Controllers/PersonaController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IDL2_WEB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+
+        _dbContext _db = new _dbContext();
+
+        [HttpGet]
+        public IActionResult ListarTodo()
+        {
+            //select * from persona
+            return Ok(_db.Personas.ToList());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObtenerPorId(int id)
+        {
+            //select * from persona where id = {id}
+            return Ok(_db.Personas.Find(id));
+        }
+
+        [HttpPost]
+        public IActionResult Insertar([FromBody] Persona request )
+        {
+            //insert  into personas (declaro todos los campos) ==> ingreso todos los valores
+            _db.Personas.Add(request);
+            _db.SaveChanges();
+            return Ok(request);
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody] Persona request)
+        {
+            //update persona set nombre = request.nombre, apellido_paterno = request.ApellidoPaterno where id = request.id
+            _db.Personas.Update(request);
+            _db.SaveChanges();
+            return Ok(request);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            int rowAfected = 0;
+            Persona Per = _db.Personas.Find(id);
+
+            if (Per != null)
+            {
+                _db.Personas.Remove(Per);
+                rowAfected = _db.SaveChanges();
+            }
+
+            return Ok($"Numero de Registro Eliminado {rowAfected}");
+        }
+
+
+    }
+}
---
+++ IDL2_WEB/Persona.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace IDL2_WEB;
+
+[Table("persona")]
+public partial class Persona
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("nombre")]
+    [StringLength(30)]
+    [Unicode(false)]
+    public string? Nombre { get; set; }
+
+    [Column("apellido_paterno")]
+    [StringLength(30)]
+    [Unicode(false)]
+    public string? ApellidoPaterno { get; set; }
+
+    [Column("apellido_materno")]
+    [StringLength(30)]
+    [Unicode(false)]
+    public string? ApellidoMaterno { get; set; }
+
+    [Column("fecha_nacimiento")]
+    public DateOnly? FechaNacimiento { get; set; }
+}
