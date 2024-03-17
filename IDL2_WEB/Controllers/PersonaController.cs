3970976f85467189856f611f92b6cdc1afac9a8d
AGAT89 <aariastevez@gmail.com>
2024-03-17 00:37:31 -0500

    Se agrego comentarios a Persona.Controller.cs

--- IDL2_WEB/Controllers/PersonaController.cs
+++ IDL2_WEB/Controllers/PersonaController.cs
@@ -7,56 +7,65 @@ namespace IDL2_WEB.Controllers
     [ApiController]
     public class PersonaController : ControllerBase
     {
-
+        // Se crea una instancia de DbContext para interactuar con la base de datos
         _dbContext _db = new _dbContext();
 
+        // Método HTTP GET para listar todas las personas
         [HttpGet]
         public IActionResult ListarTodo()
         {
-            //select * from persona
+            // Obtiene todas las personas de la base de datos y las devuelve como respuesta
             return Ok(_db.Personas.ToList());
         }
 
+        // Método HTTP GET para obtener una persona por su ID
         [HttpGet("{id}")]
         public IActionResult ObtenerPorId(int id)
         {
-            //select * from persona where id = {id}
+            // Busca una persona por su ID en la base de datos y la devuelve como respuesta
             return Ok(_db.Personas.Find(id));
         }
 
+        // Método HTTP POST para insertar una nueva persona
         [HttpPost]
-        public IActionResult Insertar([FromBody] Persona request )
+        public IActionResult Insertar([FromBody] Persona request)
         {
-            //insert  into personas (declaro todos los campos) ==> ingreso todos los valores
+            // Inserta una nueva persona en la base de datos y la devuelve como respuesta
             _db.Personas.Add(request);
             _db.SaveChanges();
             return Ok(request);
         }
 
+        // Método HTTP PUT para actualizar una persona existente
         [HttpPut]
         public IActionResult Update([FromBody] Persona request)
         {
-            //update persona set nombre = request.nombre, apellido_paterno = request.ApellidoPaterno where id = request.id
+            // Actualiza la información de una persona en la base de datos y la devuelve como respuesta
             _db.Personas.Update(request);
             _db.SaveChanges();
             return Ok(request);
         }
 
+        // Método HTTP DELETE para eliminar una persona por su ID
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
+            // Se inicializa el contador de filas afectadas
             int rowAfected = 0;
+            // Se busca la persona por su ID en la base de datos
             Persona Per = _db.Personas.Find(id);
 
+            // Si se encuentra la persona en la base de datos
             if (Per != null)
             {
+                // Se elimina la persona de la base de datos
                 _db.Personas.Remove(Per);
+                // Se guarda el número de filas afectadas al realizar la eliminación
                 rowAfected = _db.SaveChanges();
             }
 
+            // Se devuelve un mensaje indicando el número de registros eliminados
             return Ok($"Numero de Registro Eliminado {rowAfected}");
         }
-
-
     }
 }
